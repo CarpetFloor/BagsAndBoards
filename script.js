ef8dcf3b625b7dcd4ae50c1add653615caef9526
Dylan Bartness <dylan.bartness@gmail.com>
2022-10-03 22:34:05 -0500

    switched to Babylon

--- script.js
+++ script.js
@@ -1,25 +1,53 @@
-const c = document.getElementById("canvas");
-const renderer = new THREE.WebGLRenderer({c});
-let w = c.width;
-let h = c.height;
+const canvas = document.getElementById("canvas"); // Get the canvas element
+const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
 
-const fov = 75;
-const aspect = 2;
-const near = 0.1;
-const far = 5;
-const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.z = 2;
+const createScene = function () {
+    // Creates a basic Babylon Scene object
+    const scene = new BABYLON.Scene(engine);
 
-const scene = new THREE.Scene();
-const boxWidth = 1;
-const boxHeight = 1;
-const boxDepth = 1;
-const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+    // Creates and positions a free camera
+    const camera = new BABYLON.FreeCamera("camera1", 
+        new BABYLON.Vector3(0, 5, -10), scene);
+    
+    // Targets the camera to scene origin
+    camera.setTarget(BABYLON.Vector3.Zero());
+    
+    // This attaches the camera to the canvas
+    camera.attachControl(canvas, true);
+    
+    // Creates a light, aiming 0,1,0 - to the sky
+    const light = new BABYLON.HemisphericLight("light", 
+        new BABYLON.Vector3(0, 1, 0), scene);
+    
+    // Dim the light a small amount - 0 to 1
+    light.intensity = 0.7;
+    // Built-in 'sphere' shape.
+    
+    const sphere = BABYLON.MeshBuilder.CreateSphere("sphere", 
+        {diameter: 2, segments: 32}, scene);
+    
+    // Move the sphere upward 1/2 its height
+    sphere.position.y = 1;
 
-const material = new THREE.MeshBasicMaterial({color: 0x44aa88});
+    // Built-in 'ground' shape.
+    const ground = BABYLON.MeshBuilder.CreateGround("ground", 
+        {width: 8, height: 8}, scene);
+    
+    let groundMaterial = new BABYLON.StandardMaterial("Ground Material", scene);
+    ground.material = groundMaterial;
+    ground.material.diffuseColor = BABYLON.Color3.Red();
+    
+    return scene;
+};
 
-const cube = new THREE.Mesh(geometry, material);
+const scene = createScene(); //Call the createScene function
 
-scene.add(cube);
+// Register a render loop to repeatedly render the scene
+engine.runRenderLoop(function () {
+    scene.render();
+});
 
-renderer.render(scene, camera);
+// Watch for browser/canvas resize events
+window.addEventListener("resize", function () {
+    engine.resize();
+});
