e95941d77e6cd0026c59e16c94ef13905d0711c2
CarpetFloor <dylan.bartness@gmail.com>
2022-10-03 20:40:52 -0500

    different basic code

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-let w = window.innerWidth;
-let h = window.innerHeight;
+const c = document.getElementById("canvas");
+const renderer = new THREE.WebGLRenderer({c});
+let w = c.width;
+let h = c.height;
+
+const fov = 75;
+const aspect = 2;
+const near = 0.1;
+const far = 5;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.z = 2;
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
+const boxWidth = 1;
+const boxHeight = 1;
+const boxDepth = 1;
+const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(w, h);
-document.body.appendChild(renderer.domElement);
+const material = new THREE.MeshBasicMaterial({color: 0x44aa88});
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
 const cube = new THREE.Mesh(geometry, material);
+
 scene.add(cube);
 
-camera.position.z = 5;
-
-const FPS = 30;
-function animate() {
-    renderer.render(scene, camera);
-}
-window.setInterval(animate, 1000 / FPS);
+renderer.render(scene, camera);
